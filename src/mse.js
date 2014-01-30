87b32a697c6c09cf56aef564df1727ead48e5b66
Rob Griffiths <rob@bytespider.eu>
2014-01-30 08:47:34 +0000

    added step to skip x number of bytes. ideal for larger data sets

--- src/mse.js
+++ src/mse.js
@@ -1,10 +1,11 @@
-module.exports = function mse(original, noisy) {
+module.exports = function mse(original, noisy, step) {
     var error
     var offset;
     var sum = 0;
     var i = 0;
+    step = step || 1;
 
-    for (i = 0; i < original.length; i += 1) {
+    for (i = 0; i < original.length; i += step) {
         error = original[i] - noisy[i];
         sum += error * error;
     }
